4a429ee40b16bca5eeacb26da063a2243894e7d9
Anton Prince Oommen <antonprince95@gmail.com>
2026-05-27 16:00:01 +0530

    reel data processing

--- test.py
+++ test.py
@@ -1,28 +1,30 @@
 import json
-
-with open("result_from_llm.json","r") as f:
-    raw = json.load(f)
+import requests
 
 
-with open("scraped_data_instagram_hashtag_scraper.json","r") as f:
-    master_data = json.load(f)
+# with open("result_from_llm.json","r") as f:
+#     raw = json.load(f)
+
+
+# with open("scraped_data_instagram_hashtag_scraper.json","r") as f:
+#     master_data = json.load(f)
 
 
 
-print(f"Count of response from llm is  {len(raw)}")
-print(f"Count of master data is {len(master_data)}")
+# print(f"Count of response from llm is  {len(raw)}")
+# print(f"Count of master data is {len(master_data)}")
 
-master_ids = {item.get("id") for item in master_data}
-raw_ids = { item.get("id") for item in raw}
+# master_ids = {item.get("id") for item in master_data}
+# raw_ids = { item.get("id") for item in raw}
 
 
-# for item in raw:
-#     if item.get("id") in master_ids:
-#         print(f"Found match for id: {item.get('id')}")
-#     else:
-#         print(f"No match found for id: {item.get('id')}")
+# # for item in raw:
+# #     if item.get("id") in master_ids:
+# #         print(f"Found match for id: {item.get('id')}")
+# #     else:
+# #         print(f"No match found for id: {item.get('id')}")
 
-raw_ids = set(raw_ids)
-master_ids = set(master_ids)
-print(f"IDs in raw but not in master: {master_ids - raw_ids }")
-print(f"Count of raw id: {len(raw_ids)} and count of master id: {len(master_ids)}")
+# raw_ids = set(raw_ids)
+# master_ids = set(master_ids)
+# print(f"IDs in raw but not in master: {master_ids - raw_ids }")
+# print(f"Count of raw id: {len(raw_ids)} and count of master id: {len(master_ids)}")
