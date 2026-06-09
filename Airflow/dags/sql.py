67a39d8ae49daa220416c4267380fe50eac97ca9
antonnprince <antonprince95@gmail.com>
2026-06-09 16:01:13 +0530

    sql query update

--- Airflow/dags/sql.py
+++ Airflow/dags/sql.py
@@ -2,7 +2,7 @@ from airflow import DAG
 from airflow.decorators import task
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from datetime import datetime,timedelta
-
+import random
 
 POSTGRES_CONN_ID = 'postgres_test'
 
@@ -18,7 +18,7 @@ with DAG(dag_id = 'sql_test', schedule = "@daily", default_args = default_args, 
         conn = pg_hook.get_conn()
         cursor = conn.cursor()
 
-        cursor.execute("""CREATE TABLE test_for_airflow 
+        cursor.execute("""CREATE TABLE IF NOT EXISTS test_for_airflow 
         (id INT PRIMARY KEY, 
         name TEXT,
         email TEXT
@@ -29,13 +29,15 @@ with DAG(dag_id = 'sql_test', schedule = "@daily", default_args = default_args, 
 
     @task
     def insert_values():
+        
         pg_hook = PostgresHook(postgres_conn_id = POSTGRES_CONN_ID)
         conn = pg_hook.get_conn()
         cursor = conn.cursor()
 
-
-        cursor.execute("""
-        INSERT INTO test_for_airflow VALUES(1,'aaaaaa','BBBBBBB'),(2,'b','cccccccccccccc')
+        num1 = random.randint(1,100)
+        num2 = random.randint(5,100)
+        cursor.execute(f"""
+        INSERT INTO test_for_airflow VALUES({num1},'aaaaaa','BBBBBBB'),({num2},'b','cccccccccccccc')
         """)
 
         conn.commit()
